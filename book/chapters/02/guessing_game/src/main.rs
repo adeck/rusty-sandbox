69a7c00df638ff30fba2dd70241c6d595a449525
garin <nope@nope>
2022-04-21 11:11:31 -0700

    added loop + integer parsing

--- book/chapters/02/guessing_game/src/main.rs
+++ book/chapters/02/guessing_game/src/main.rs
@@ -5,14 +5,23 @@ use rand::Rng;
 
 fn main() {
     let secret_num: u8 = rand::thread_rng().gen_range(1..=100);
-    println!("Secret number is: {}", secret_num);
     println!("This is a guessing game!");
-    println!("Make a guess...");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Must enter a valid string");
-    println!("You guessed: {}", guess);
+    'guessing: loop {
+        println!("Secret number is: {}", secret_num);
+        println!("Make a guess...");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Must enter a valid string");
+        let guess: u8 = match guess.trim().parse() {
+            Err(e) => {
+                println!("Enter a valid number. Error: {}", e);
+                continue 'guessing;
+            },
+            Ok(v) => v,
+        };
+        println!("You guessed: {}", guess);
+    }
 }
 
 
