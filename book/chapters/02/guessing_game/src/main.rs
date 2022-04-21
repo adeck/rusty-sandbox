c9945421721f5b77d90c1e5a51526e310b79007b
garin <nope@nope>
2022-04-21 11:05:06 -0700

    added line reading

--- book/chapters/02/guessing_game/src/main.rs
+++ book/chapters/02/guessing_game/src/main.rs
@@ -1,11 +1,18 @@
 
-// use std::io;
+use std::io;
 use rand::Rng;
 
 
 fn main() {
-    let secret_num = rand::thread_rng().gen_range(1..=100);
+    let secret_num: u8 = rand::thread_rng().gen_range(1..=100);
     println!("Secret number is: {}", secret_num);
+    println!("This is a guessing game!");
+    println!("Make a guess...");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Must enter a valid string");
+    println!("You guessed: {}", guess);
 }
 
 
