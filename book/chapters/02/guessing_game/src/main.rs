7490c3aa02d1eb4024853ad32b4437ecfa91d849
garin <nope@nope>
2022-04-26 17:36:12 -0700

    reimplemented guessing game

--- book/chapters/02/guessing_game/src/main.rs
+++ book/chapters/02/guessing_game/src/main.rs
@@ -4,45 +4,41 @@ use rand::Rng;
 use std::io;
 use std::cmp::Ordering;
 
-
-
 fn main() {
     let secret_num: u8 = rand::thread_rng().gen_range(1..=100);
-    println!("This is a guessing game.");
     'guessing: loop {
-        println!("Secret num: {}", secret_num);
-        let guess: u8 = match make_guess() {
+        println!("Secret number: {}", secret_num);
+        let guess = match make_guess() {
             Some(v) => v,
             None => continue,
         };
         match guess.cmp(&secret_num) {
-            Ordering::Less => println!("Too low."),
+            Ordering::Less => println!("Too low"),
             Ordering::Equal => break 'guessing,
-            Ordering::Greater => println!("Too high."),
+            Ordering::Greater => println!("Too high"),
         }
     }
-    println!("You guessed correctly!");
-    println!("You won the game! Congratulations!");
+    println!("Correct!");
+    println!("You won! Congratulations!");
 }
-
 
 fn make_guess() -> Option<u8> {
     println!("Make a guess!");
-    let mut guess = String::new();
+    let mut guess: String = String::new();
     io::stdin()
         .read_line(&mut guess)
-        .expect("Error attempting to read line");
-    match guess.trim().parse() {
-        Ok(v) => if v < 101 {
-            Some(v)
-        } else {
-            println!("Must guess a number in the range [1, 100].");
-            None
-        },
+        .expect("Failed to read line");
+    let guess: u8 = match guess.trim().parse() {
+        Ok(v) => v,
         Err(e) => {
-            println!("Must guess a valid number. Error: {}", e);
-            None
+            println!("Value not a valid number. Error was: {}", e);
+            return None;
         },
+    };
+    if guess > 100 {
+        println!("Value must be in range [1, 100]");
+        return None;
     }
+    return Some(guess);
 }
 
