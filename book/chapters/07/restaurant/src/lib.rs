d29a4743ab82dc9723c3c13a0333c7b5fab076d4
garin <nope@nope>
2022-04-28 15:06:37 -0700

    continuing chapter 7...

--- book/chapters/07/restaurant/src/lib.rs
+++ book/chapters/07/restaurant/src/lib.rs
@@ -4,6 +4,12 @@ fn main() {
     front_of_house::hosting::add_to_waitlist();
 }
 
+fn eat_at_restaurant() {
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please.", meal.toast);
+}
+
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
@@ -18,4 +24,18 @@ mod front_of_house {
     }
 }
 
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Self {
+            Self {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peach"),
+            }
+        }
+    }
+}
 
