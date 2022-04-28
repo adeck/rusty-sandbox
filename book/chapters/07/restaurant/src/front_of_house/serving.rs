3730e704f51d07d8c221abe4484a4ed8a0c3ad25
garin <nope@nope>
2022-04-28 16:47:34 -0700

    rest of chapter 7

---
+++ book/chapters/07/restaurant/src/front_of_house/serving.rs
@@ -0,0 +1,6 @@
+
+fn take_order() {}
+fn serve_order() {}
+fn bus_table() {}
+fn take_payment() {}
+
--- book/chapters/07/restaurant/src/lib.rs
+++ book/chapters/07/restaurant/src/lib.rs
@@ -1,27 +1,18 @@
 
+mod front_of_house;
+pub use front_of_house::hosting;
+
 fn main() {
     crate::front_of_house::hosting::add_to_waitlist();
     front_of_house::hosting::add_to_waitlist();
+    // allowed because of the import
+    hosting::add_to_waitlist();
 }
 
 fn eat_at_restaurant() {
     let mut meal = back_of_house::Breakfast::summer("Rye");
     meal.toast = String::from("Wheat");
     println!("I'd like {} toast please.", meal.toast);
-}
-
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-        fn seat_table() {}
-    }
-
-    pub mod serving {
-        fn take_order() {}
-        fn serve_order() {}
-        fn bus_table() {}
-        fn take_payment() {}
-    }
 }
 
 mod back_of_house {
